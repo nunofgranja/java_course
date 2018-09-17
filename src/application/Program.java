462d4feb3396c1e8bb6154a3b8894d5df53ea662
Nuno Granja <nuno.barbosa.granja@celfocus.com>
2018-09-17 15:48:09 +0100

    Exercicio de Fixação
    - Added entity BankAccount

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+ System.out.println("commit granja");
+		System.out.print("What is sthe dollar price?");
+
+		CurrencyConverter.currencyPrice = sc.nextDouble();
+		
+		System.out.print("How many dollars will be bought?");
+		
+		System.out.printf("Amount to be paid in reais = %.2f", CurrencyConverter.calculateTotalPurchasePrice(sc.nextDouble()));
+		
+
+		sc.close();
+	}
+
+}
---
+++ src/entities/BankAccount.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class BankAccount {
+
+	private String name;
+	private int number;
+	private double balance;
+	private double tax = 5.00;
+
+	public BankAccount(String name, int number, double balance) {
+		super();
+		this.name = name;
+		this.number = number;
+		this.balance = balance;
+	}
+	
+	public BankAccount(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+
+	public void addBalance(double value) {
+
+		balance += value;
+
+	}
+
+	public void withdrawBalance(double value) {
+
+		balance -= value - tax;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+}
