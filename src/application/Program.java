3ed9c655148b46b47049b056eebb3aa53a335fba
Nuno Granja <nuno.barbosa.granja@celfocus.com>
2018-09-25 15:51:10 +0100

    Array Exercise

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,66 +1,49 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
-import entities.Employee3;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Employee3 employee;
-		List<Employee3> list = new ArrayList<>();
 
-		System.out.print("How many employees will be registered? ");
+		int m = sc.nextInt();
 		int n = sc.nextInt();
-		int i = 0;
 
-		for (i = 0; i < n; i++) {
-			System.out.println("Employee #" + i + 1 +  ":");
-			System.out.print("ID:");
-			Integer id = sc.nextInt();
-			System.out.print("Name:");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Salary :");
-			Double salary = sc.nextDouble();
+		int[][] mat = new int[m][n];
 
-			employee = new Employee3(id, name, salary);
-
-			list.add(employee);
-
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
 		}
 
-		System.out.print("Enter the employee id that will have salary increase:");
-		Integer id = sc.nextInt();
-		System.out.print("Enter the percentage:");
-		Double raisePercent = sc.nextDouble();
+		int pivot = sc.nextInt();
 
-		
-		Employee3 emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		if (emp == null) {
-			System.out.println("This id does not exist!");
-		}
-		else {
-			System.out.print("Enter the percentage: ");
-			double percentage = sc.nextDouble();
-			emp.increaseSalary(raisePercent);
-		}
-
-		System.out.println("List of employees: ");
-
-		for (Employee3 x : list)
-
-		{
-			System.out.println(x);
-
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == pivot) {
+					System.out.println("Position " + i + "," + j + ":");
+				if (j - 1 >= 0) {
+					System.out.println("Left: " + mat[i][j - 1]);
+				}
+				if (i - 1 >= 0) {
+					System.out.println("Up: " + mat[i-1][j]);
+				}
+				if (j + 1 < mat[i].length) {
+					System.out.println("Right: " + mat[i][j+1]);
+				}
+				if (i + 1 < mat.length) {
+					
+					System.out.println("Down: " + mat[i+1][j]);
+				}
+				}
+			}
 		}
 
 		sc.close();
+
 	}
 
 }
---
+++ src/application/ProgramEmployee3.java
@@ -0,0 +1,66 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee3;
+
+public class ProgramEmployee3 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Employee3 employee;
+		List<Employee3> list = new ArrayList<>();
+
+		System.out.print("How many employees will be registered? ");
+		int n = sc.nextInt();
+		int i = 0;
+
+		for (i = 0; i < n; i++) {
+			System.out.println("Employee #" + i + 1 +  ":");
+			System.out.print("ID:");
+			Integer id = sc.nextInt();
+			System.out.print("Name:");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary :");
+			Double salary = sc.nextDouble();
+
+			employee = new Employee3(id, name, salary);
+
+			list.add(employee);
+
+		}
+
+		System.out.print("Enter the employee id that will have salary increase:");
+		Integer id = sc.nextInt();
+		System.out.print("Enter the percentage:");
+		Double raisePercent = sc.nextDouble();
+
+		
+		Employee3 emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (emp == null) {
+			System.out.println("This id does not exist!");
+		}
+		else {
+			System.out.print("Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(raisePercent);
+		}
+
+		System.out.println("List of employees: ");
+
+		for (Employee3 x : list)
+
+		{
+			System.out.println(x);
+
+		}
+
+		sc.close();
+	}
+
+}
